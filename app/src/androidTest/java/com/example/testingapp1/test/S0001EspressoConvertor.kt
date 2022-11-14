b666840f668652ca7bdaae217312217dc7255daa
davidg <dogriffiths@gmail.com>
2022-11-14 08:14:59 +0000

    Changes: Mon 14 Nov 2022 08:14:59 GMT

--- app/src/androidTest/java/com/example/testingapp1/test/S0001EspressoConvertor.kt
+++ app/src/androidTest/java/com/example/testingapp1/test/S0001EspressoConvertor.kt
@@ -1,8 +1,14 @@
 package com.example.testingapp1.test
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
+import com.example.testingapp1.R.id
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,7 +20,9 @@ class S0001EspressoConvertor {
     @Test
     fun shouldDoAnEspressoConversionProperly() {
         // Given I have entered -40 into the celsius field
+        onView(withId(id.celsius)).perform(replaceText("-40"))
         // When I click the 'Convert' button
+        onView(withText("Convert")).perform(click())
         // Then I will see -40 in the fahrenheit field
     }
 }
