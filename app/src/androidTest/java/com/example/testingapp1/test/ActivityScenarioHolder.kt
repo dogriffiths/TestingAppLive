840d5d0a3ac71e464c14ca52190cdc239e8932bf
davidg <dogriffiths@gmail.com>
2022-11-14 09:14:55 +0000

    Changes: Mon 14 Nov 2022 09:14:55 GMT

--- app/src/androidTest/java/com/example/testingapp1/test/ActivityScenarioHolder.kt
+++ app/src/androidTest/java/com/example/testingapp1/test/ActivityScenarioHolder.kt
@@ -3,11 +3,17 @@ package com.example.testingapp1.test
 import android.app.Activity
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
+import io.cucumber.java.After
 
 class ActivityScenarioHolder {
     private var scenario: ActivityScenario<*>? = null
 
     fun launch(intent: Intent) {
         scenario = ActivityScenario.launch<Activity>(intent)
+    }
+
+    @After
+    fun close() {
+        scenario?.close()
     }
 }
