1a64d6e593966811220ca2b7fe86073bc4d85296
davidg <dogriffiths@gmail.com>
2022-11-14 08:49:47 +0000

    Changes: Mon 14 Nov 2022 08:49:47 GMT

--- app/src/androidTest/java/com/example/testingapp1/test/S0003ComposeConvertorComponent.kt
+++ app/src/androidTest/java/com/example/testingapp1/test/S0003ComposeConvertorComponent.kt
@@ -3,6 +3,7 @@ package com.example.testingapp1.test
 import android.view.View
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
@@ -15,6 +16,7 @@ import com.example.testingapp1.MainActivity
 import org.junit.Test
 import org.junit.runner.RunWith
 import com.example.testingapp1.R.id
+import com.example.testingapp1.ui.converter.ComposeConverter
 import org.hamcrest.Matcher
 import org.junit.Before
 import org.junit.Rule
@@ -25,9 +27,16 @@ import org.junit.Rule
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class S0002ComposeConvertor {
+class S0003ComposeConvertorComponent {
     @get:Rule
-    val testRule = createAndroidComposeRule<MainActivity>()
+    val testRule = createComposeRule()
+
+    @Before
+    fun setUpComponent() {
+        testRule.setContent {
+            ComposeConverter()
+        }
+    }
 
     @Test
     fun shouldDoAComposeConversionProperly() {
