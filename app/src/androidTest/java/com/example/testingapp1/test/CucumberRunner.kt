6438c736f2941495ee061087105c9535586d4d98
davidg <dogriffiths@gmail.com>
2022-11-14 09:08:59 +0000

    Changes: Mon 14 Nov 2022 09:08:59 GMT

---
+++ app/src/androidTest/java/com/example/testingapp1/test/CucumberRunner.kt
@@ -0,0 +1,45 @@
+package com.example.testingapp1.test
+
+import android.os.Bundle
+import io.cucumber.android.runner.CucumberAndroidJUnitRunner
+import io.cucumber.junit.CucumberOptions
+import java.io.File
+
+@CucumberOptions(features = ["features"], strict = true)
+class CucumberRunner : CucumberAndroidJUnitRunner() {
+    override fun onCreate(bundle: Bundle) {
+        bundle.putString("plugin", pluginConfigurationString)
+        File(absoluteFilesPath).mkdirs()
+        super.onCreate(bundle)
+    }
+
+    private val pluginConfigurationString: String
+        private get() {
+            val cucumber = "cucumber"
+            val separator = "--"
+            return "junit:" + getCucumberXml(cucumber) + separator +
+                    "html:" + getCucumberHtml(cucumber)
+        }
+
+    private fun getCucumberHtml(cucumber: String): String {
+        return "$absoluteFilesPath/$cucumber.html"
+    }
+
+    private fun getCucumberXml(cucumber: String): String {
+        return "$absoluteFilesPath/$cucumber.xml"
+    }
+
+    /**
+     * The path which is used for the report files.
+     *
+     * @return the absolute path for the report files
+     */
+    private val absoluteFilesPath: String
+        private get() {
+
+            // Stored testing device at /sdcard/Android/data/com.example.testingapp1
+            val directory = targetContext.getExternalFilesDir(null)
+            return File(directory, "reports").absolutePath
+        }
+}
+
