d0e1e59791d11726733e760bca2e4a454719cbab
davidg <dogriffiths@gmail.com>
2022-11-14 09:13:50 +0000

    Changes: Mon 14 Nov 2022 09:13:50 GMT

--- app/src/androidTest/java/com/example/testingapp1/test/ActivityScenarioHolder.kt
+++ app/src/androidTest/java/com/example/testingapp1/test/ActivityScenarioHolder.kt
@@ -1,4 +1,13 @@
 package com.example.testingapp1.test
 
+import android.app.Activity
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+
 class ActivityScenarioHolder {
+    private var scenario: ActivityScenario<*>? = null
+
+    fun launch(intent: Intent) {
+        scenario = ActivityScenario.launch<Activity>(intent)
+    }
 }
