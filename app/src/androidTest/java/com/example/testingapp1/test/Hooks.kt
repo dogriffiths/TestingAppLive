372256b5e01b32d2bf69e6d6543ea9ff9069d5e7
davidg <dogriffiths@gmail.com>
2022-11-14 10:33:38 +0000

    Changes: Mon 14 Nov 2022 10:33:38 GMT

--- app/src/androidTest/java/com/example/testingapp1/test/Hooks.kt
+++ app/src/androidTest/java/com/example/testingapp1/test/Hooks.kt
@@ -1,4 +1,14 @@
 package com.example.testingapp1.test
 
+import io.cucumber.java.Before
+import okhttp3.mockwebserver.MockWebServer
+
+var mockServer: MockWebServer? = null
+
 class Hooks {
+    @Before
+    fun startServer() {
+        mockServer = MockWebServer()
+        mockServer?.start(8080)
+    }
 }
