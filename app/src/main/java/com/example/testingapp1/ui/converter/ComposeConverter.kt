bb398e81cfde4dcb129abdcd62985f29e21a1572
davidg <dogriffiths@gmail.com>
2022-11-14 08:47:25 +0000

    Changes: Mon 14 Nov 2022 08:47:25 GMT

--- app/src/main/java/com/example/testingapp1/ui/converter/ComposeConverter.kt
+++ app/src/main/java/com/example/testingapp1/ui/converter/ComposeConverter.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import java.lang.Double.parseDouble
 
 private fun isNumeric(s: String): Boolean {
@@ -54,4 +55,10 @@ fun ComposeConverter() {
             text = fahrenheit.toString()
         )
     }
+}
+
+@Preview
+@Composable
+fun PreviewComposeConverter() {
+    ComposeConverter()
 }
